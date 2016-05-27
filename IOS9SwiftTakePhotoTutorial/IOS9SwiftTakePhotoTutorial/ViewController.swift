b05171b2eadf6da3ba16a0fde357b3de8d0e24d4
DarianNwankwo <dnwankwo404@gmail.com>
2016-05-26 23:10:37 -0400

    First feature added for camera usability

---
+++ IOS9SwiftTakePhotoTutorial/IOS9SwiftTakePhotoTutorial/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  IOS9SwiftTakePhotoTutorial
+//
+//  Created by Darian Nwankwo on 5/26/16.
+//  Copyright © 2016 Darian Nwankwo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    var imagePicker: UIImagePickerController!
+    @IBOutlet var imageView: UIImageView!
+    @IBAction func takePhoto(sender: UIButton) {
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .Camera
+        
+        presentViewController(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        imagePicker.dismissViewControllerAnimated(true, completion: nil)
+        imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+    }
+}
+
